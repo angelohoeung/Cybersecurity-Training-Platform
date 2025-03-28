8a3f1b51581ed88b76182c64a56be6af9c242c36
Angie Doka <148018574+angiedoka@users.noreply.github.com>
2025-03-27 20:59:19 -0400

    Update buttons EmailspoofDemo6.jsx

--- src/pages/EmailspoofDemo6.jsx
+++ src/pages/EmailspoofDemo6.jsx
@@ -95,8 +95,11 @@ const EmailspoofDemo6 = ({ token }) => {
         </div>
       </div>
       
-      <button onClick={handleNext} className="button">Continue</button>
-      <button onClick={handleHome} className="button">Return to Home</button>
+      <div style={{ display: "flex", flexDirection: "column", alignItems: "center", gap: "5px", marginTop: "20px" }}>
+        <button onClick={handleNext} className="button" style={{ width: "900px", height: "37px", fontSize: "14px", textAlign: "center" }}>Continue</button>
+        <button onClick={handleHome} className="button" style={{ width: "900px", height: "37px", fontSize: "14px", textAlign: "center" }}>Return Home</button>
+      </div>
+      
     </div>
   );
 };
